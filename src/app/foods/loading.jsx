881cd4b774898d02385df26a0d9a5b04343c3bf7
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-18 07:44:48 +0600

    Added FoodCardSkeleton To Loading Component

--- src/app/foods/loading.jsx
+++ src/app/foods/loading.jsx
@@ -1,9 +1,12 @@
+import FoodCardSkeleton from '@/components/skeletons/FoodCardSkeleton';
 import React from 'react';
 
 const loading = () => {
     return (
-        <div>
-            Loading...
+        <div className='grid my-5 grid-cols-1 md:grid-cols-3 gap-5'>
+            {
+                [...Array(12)].map((_, index) => <FoodCardSkeleton key={index} />)
+            }
         </div>
     );
 };
