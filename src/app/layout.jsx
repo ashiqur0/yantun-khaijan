ed09658d71b52e1fa0b050a8d66efdca2ecd6664
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-18 07:24:13 +0600

    Addd Navbar Component To Main Layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
+        <Navbar />
 
         <main className='px-5 py-8'>
           {children}
