f831c184c5f4ba8fffbe673d7f2331b1f5d4bf99
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-23 07:06:30 +0600

    Create addToCart Function And Share It To CartProvider As Global Variable

--- src/Provider/CartProvider.jsx
+++ src/Provider/CartProvider.jsx
@@ -1,8 +1,19 @@
 import { CartContext } from "@/context/CartContext";
+import { useState } from "react";
 
 const CartProvider = ({ children }) => {
+
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (item) => {
+        setCart([item, ...cart]);
+    }
+
+    const cartInfo = {
+        addToCart, cart,
+    }
     
-    return <CartContext>{children}</CartContext>
+    return <CartContext value={cartInfo}>{children}</CartContext>
 };
 
 export default CartProvider;
