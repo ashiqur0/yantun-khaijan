a6af84767d7175086fe1016dde351034fcb02a94
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-23 07:14:34 +0600

    Use The cart Data In CartItems Component

--- src/app/foods/CartItems.jsx
+++ src/app/foods/CartItems.jsx
@@ -1,11 +1,14 @@
 'use client'
 
-import React from 'react';
+import React, { useContext } from 'react';
 
 const CartItems = () => {
+
+    const {addToCart, cart} = useContext();
+
     return (
         <div>
-            {0} items added
+            {cart.length} items added
         </div>
     );
 };
