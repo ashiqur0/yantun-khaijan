46bf250e6d5bd2921224423b7f611f5157ba2963
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-23 17:58:45 +0600

    Create InputSearch Component

---
+++ src/components/InputSearch.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const InputSearch = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" placeholder='Search Food'></input>
+                <button>Search</button>
+            </form>
+        </div>
+    );
+};
+
+export default InputSearch;
