7c90e850bb22fff63e6af7b7477a26044abc4612
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-23 06:52:21 +0600

    Create CartItems Component To Keep Track Of Each Items Added To Cart By User

---
+++ src/app/foods/CartItems.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react';
+
+const CartItems = () => {
+    return (
+        <div>
+            {0} items added
+        </div>
+    );
+};
+
+export default CartItems;
--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,5 +1,6 @@
 import FoodCard from '@/components/cards/FoodCard';
 import React from 'react';
+import CartItems from './CartItems';
 
 const getFoods = async () => {
     const res = await fetch(' https://taxi-kitchen-api.vercel.app/api/v1/foods/random');
@@ -18,10 +19,17 @@ const FoodsPage = async () => {
         <div>
             <h2 className='text-4xl font-bold'>Total <span className='text-yellow-500'>{foods.length}</span> Foods Found.</h2>
 
-            <div className='grid my-5 grid-cols-1 md:grid-cols-3 gap-5'>
-                {
-                    foods.map(food => <FoodCard key={food.id} food={food}></FoodCard>)
-                }
+            <div className='flex gap-5'>
+                <div className='grid my-5 grid-cols-1 md:grid-cols-3 gap-5'>
+                    {
+                        foods.map(food => <FoodCard key={food.id} food={food}></FoodCard>)
+                    }
+                </div>
+
+                <div className='w-120 border-2 rounded-xl p-4'>
+                    <h2 className='text-2xl font-bold border-b'>Cart Items</h2>
+                    <CartItems></CartItems>
+                </div>
             </div>
         </div>
     );
