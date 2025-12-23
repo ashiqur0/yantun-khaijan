735c6e0d2d36ff53e3e88099c0ebc0aadf2f37e9
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-23 18:21:43 +0600

    Update InputSearch Component

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 const InputSearch = () => {
+
+    const handleInputSearch = (e) => {
+        e.preventDefault();
+        const search = e.target.search.value;
+        console.log({ search });
+    }
+
     return (
         <div>
-            <form>
-                <input type="text" placeholder='Search Food'></input>
-                <button>Search</button>
+            <form onSubmit={handleInputSearch}>
+                <input
+                    name='search'
+                    type="text"
+                    placeholder='Search Food'
+                    className='px-1 py-3 border-2 rounded w-2xl'
+                />
+
+                <button type='submit' className='px-4 py-2 bg-yellow-500 text-black rounded cursor-pointer'>Search</button>
             </form>
         </div>
     );
