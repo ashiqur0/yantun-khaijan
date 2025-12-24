4aaea31ccb61f4275f8c8ad782960be22ac1095a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-24 15:41:05 +0600

    Create Delete API To Delete A Feedback From Database

--- src/app/api/feedback/[id]/route.js
+++ src/app/api/feedback/[id]/route.js
@@ -19,3 +19,19 @@ export async function GET(request, { params }) {
     const result = await feedbackCollection.findOne(query);
     return Response.json(result);
 }
+
+// delete individual feedback using id
+export async function DELETE(request, { params }) {
+    const { id } = await params;
+
+    if (id.length != 24) {
+        return Response.json({
+            status: 400,
+            message: 'send correct id'
+        })
+    }
+
+    const query = { _id: new ObjectId(id) };
+    const result = await feedbackCollection.deleteOne(query);
+    return Response.json(result);
+}
