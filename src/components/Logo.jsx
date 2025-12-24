063cc699d1857cbe5945aff94090c01a4bc007f9
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-24 10:40:32 +0600

    Optimization: Optimize Logo Image Size At Loading Time Using Image Tag From Next Js

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Link from "next/link";
+import Image from 'next/image';
 
 const Logo = () => {
     return (
         <Link href={'/'} className='flex gap-2 items-center'>
-            <img src={'/logo.png'} className='w-15 h-auto' alt='logo'></img>
+            {/* <img src={'/logo.png'} className='w-15 h-auto' alt='logo'></img> */}
+            <Image width={90} height={90} src={'/logo.png'} className='w-15 h-auto' alt='Yantun Khaijan'/>
             <h1 className="text-4xl">Yantun Khaijan</h1>
         </Link>
     );
