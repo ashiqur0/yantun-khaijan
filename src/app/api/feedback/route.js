fbcfb95398c8f13beb990264cd08c6d4e36e741d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-24 12:19:53 +0600

    APIs: Create GET API To Get Feedback Data

---
+++ src/app/api/feedback/route.js
@@ -0,0 +1,5 @@
+import { feedback } from "../route";
+
+export async function GET(request) {
+    return Response.json(feedback);
+}
--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,3 +1,14 @@
+export const feedback = [
+    {   
+        id: 1,
+        message: "The food was delicious"
+    },
+    {   
+        id: 2,
+        message: "Waiter services improvement needed"
+    }
+]
+
 export async function GET(request) {
     return Response.json({
         status: 200,
