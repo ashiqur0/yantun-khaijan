de114b7a22c8f39a6a59c34d8e39db321d448268
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-24 10:29:06 +0600

    Optimization: Prevent Autho Loading The FoodPage Data As These Data Is Quite Heavy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <nav className='px-5 py-2 flex justify-between items-center gap-5 bg-stone-800'>
                 <Logo />
                 <div className='space-x-5'>
-                    <Link href={'/foods'} className="btn">Foods</Link>
+                    <Link prefetch={false} href={'/foods'} className="btn">Foods</Link>
                     <Link href={'/reviews'} className="btn">Reviews</Link>
                 </div>
             </nav>
