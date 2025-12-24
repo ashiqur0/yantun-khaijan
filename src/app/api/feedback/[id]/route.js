341f1c57f2a4442ceba997c46385c97b274a6bef
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-24 15:53:26 +0600

    Create PATCH API To Update A Feedback In Databse Using Id

--- src/app/api/feedback/[id]/route.js
+++ src/app/api/feedback/[id]/route.js
@@ -35,3 +35,32 @@ export async function DELETE(request, { params }) {
     const result = await feedbackCollection.deleteOne(query);
     return Response.json(result);
 }
+
+// update individual feedback using id
+export async function PATCH(request, { params }) {
+    const { id } = await params;
+    const { message } = await request.json();
+
+    if (id.length != 24) {
+        return Response.json({
+            status: 400,
+            message: 'send correct id'
+        })
+    }
+
+    if (!message || typeof message !== 'string') {
+        return Response.json({
+            status: 400,
+            message: 'Please send a message'
+        });
+    }
+
+    const query = { _id: new ObjectId(id) };
+    const newData = {
+        $set: {
+            message
+        }
+    }
+    const result = await feedbackCollection.updateOne(query, newData);
+    return Response.json(result);
+}
