47b1fc6568e9bdd107a78bdebdee648a819ca550
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-25 09:15:08 +0600

    Added The FeedbackForm Component To AddFeedback Page

--- src/app/feedbacks/add/page.jsx
+++ src/app/feedbacks/add/page.jsx
@@ -1,9 +1,11 @@
+import FeedbackForms from '@/components/forms/FeedbackForms';
 import React from 'react';
 
 const AddFeedback = () => {
     return (
         <div>
             <h1>Add Feedback</h1>
+            <FeedbackForms />
         </div>
     );
 };
