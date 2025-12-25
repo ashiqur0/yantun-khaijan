ca2f8e35962e817e98b2a6ff154aefa96eac6ff8
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-25 09:52:03 +0600

    Implement Functionalities To Collect Feedback From Clent And Store It To Database

--- src/components/forms/FeedbackForms.jsx
+++ src/components/forms/FeedbackForms.jsx
@@ -2,17 +2,38 @@
 import React from 'react';
 
 const FeedbackForms = () => {
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const message = e.target.message.value;
+
+        const res = await fetch('http://localhost:3000/api/feedback', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ message })
+        });
+        const data = await res.json();
+
+        if (data.insertedId) {
+            alert('success');
+        }
+    }
+
     return (
-        <div>
-            <form>
+        <div className='p-3'>
+            <form onSubmit={handleSubmit}
+                className='flex flex-col space-y-5'>
                 <textarea
                     className='w-xl border border-dashed'
                     name='message'
                     cols='30'
                     rows={'10'}
+                    required
                 />
 
-                <button className='btn'>Add Feedback</button>
+                <button className='btn w-80'>Add Feedback</button>
             </form>
         </div>
     );
