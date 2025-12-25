d4de0645394c62cadca0c94ac2356da2ad201d8b
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-25 12:17:08 +0600

    Added Time To Each Feedback Card

--- src/components/cards/FeedbackCard.jsx
+++ src/components/cards/FeedbackCard.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Pencil, Trash2, MessageSquare } from 'lucide-react';
 
-const FeedbackCard = ({ f}) => {
+const FeedbackCard = ({ f }) => {
     const { message, _id } = f;
 
     return (
         <div className="max-w-md bg-white border border-gray-200 rounded-xl shadow-sm p-5 hover:shadow-md transition-shadow">
             <div className="flex items-center justify-between mb-4">
-                <div className="flex items-center gap-2">
+                <div className="flex justify items-center gap-2">
                     <div className="p-2 bg-blue-50 rounded-lg text-blue-600">
                         <MessageSquare size={18} />
                     </div>
                     <span className="text-xs font-mono text-gray-400">ID: {_id.slice(-8)}</span>
+                </div>
+                <div className='text-gray-500'>
+                    {new Date(f.date).toLocaleDateString()}, {' '}
+                    {new Date(f.date).toLocaleTimeString()}
                 </div>
             </div>
 
