ff73e4ee1956fd9e1711a78ec32b5772085d87f0
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-25 11:08:55 +0600

    Show Error Status While User Will Request Invalid Path Name

--- src/proxy.js
+++ src/proxy.js
@@ -1,13 +1,24 @@
+import status from 'daisyui/components/status';
 import { NextResponse } from 'next/server'
- 
+
 // This function can be marked `async` if using `await` inside
 export function proxy(request) {
-  return NextResponse.redirect(new URL('/', request.url))
+
+    const { pathname } = request.nextUrl;
+
+    // get error status while the path name invalid
+    if (!pathname.startsWith('/api/feedback')) {
+        return NextResponse.json({
+            status: 404
+        })
+    }
+    
+//   return NextResponse.redirect(new URL('/', request.url));
 }
- 
+
 // Alternatively, you can use a default export:
 // export default function proxy(request) { ... }
- 
+
 export const config = {
-  matcher: '/about/:path*',
+    matcher: '/api/:path*',
 }
